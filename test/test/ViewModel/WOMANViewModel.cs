12e9385ad4086ca0f5d7c37f4088d1142736e25a
Torquecandy <leo755307@gmail.com>
2022-01-23 16:00:56 +0800

    First

---
+++ test/test/ViewModel/WOMANViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using test.Models;
+
+namespace test.ViewModel
+{
+    public class WOMANViewModel
+    {
+        public IEnumerable<catalogB> Cat { get; set; }
+        public IEnumerable<spu> spu { get; set; }
+
+        public WOMANViewModel()
+        {
+            shop123Entities db = new shop123Entities();
+            this.Cat = db.catalogB.ToList();            
+            this.spu = db.spu.Where(m => m.catalogAId == 1).ToList();
+
+        }
+    }
+}
